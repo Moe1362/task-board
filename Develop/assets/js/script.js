0a20df9de1b1740df96dc40466955a5a74aba084
Mohammad Abbasi <mohammad@Mohammads-MacBook-Pro.local>
2024-04-03 21:08:29 -0700

    Update script.js files and readMe file

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,37 +2,151 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
-function generateTaskId() {
+//Create a function to generate a unique task id
+const generateTaskId = () => {
+    if (nextId === null) {
+        nextId = 1;
+    } else {
+        nextId += 1;
+    }
+    localStorage.setItem("nextId", JSON.stringify(nextId));
+    return nextId;
 
 }
 
-// Todo: create a function to create a task card
-function createTaskCard(task) {
+//Create a function to create a task card
+const createTaskCard = (task) => {
+    const taskCard = $("<div>")
+        .addClass("card w-75 task-card draggable my-3")
+        .attr("data-task-id", task.id)
+    const cardHeader = $("<div>").addClass("card-header h4").text(task.title);
+    const cardBody = $("<div>").addClass("card-body");
+    const cardDescription = $("<p>").addClass("card-text").text(task.description);
+    const cardDueDate = $("<p>").addClass("card-text").text(task.dueDate);
+    const cardDeleteButton = $("<button>").addClass("btn btn-danger delete")
+        .text("Delete").attr("data-task-id", task.id);
+    cardDeleteButton.on("click", handleDeleteTask);
+
+    if (task.dueDate && task.status !== 'done') {
+        const now = dayjs();
+        const taskDueDate = dayjs(task.dueDate, "DD/MM/YYYY");
+        if (now.isSame(taskDueDate, 'day')) {
+            taskCard.addClass("bg-warning text-white");
+        } else if (now.isAfter(taskDueDate)) {
+            taskCard.addClass("bg-danger text-white");
+            cardDeleteButton.addClass("border-light");
+        }
+    }
+    cardBody.append(cardDescription, cardDueDate, cardDeleteButton);
+    taskCard.append(cardHeader, cardBody);
+
+    return taskCard;
+}
+
+// Create a function to render the task list and make cards draggable
+const renderTaskList = () => {
+    if (!taskList) {
+        taskList = [];
+    }
+    const toDoList = $("#todo-cards");
+    toDoList.empty();
+
+    const inProgressList = $("#in-progress-cards");
+    inProgressList.empty();
+
+    const doneList = $("#done-cards");
+    doneList.empty();
+
+    for (let index = 0; index < taskList.length; index++) {
+        if (taskList[index].status === "to-do") {
+            toDoList.append(createTaskCard(taskList[index]));
+
+        } else if (taskList[index].status === "in-progress") {
+            inProgressList.append(createTaskCard(taskList[index]));
+        } else if (taskList[index].status === "done") {
+            doneList.append(createTaskCard(taskList[index]));
+        }
+        $(".draggable").draggable({
+            opacity: 0.7,
+            zIndex: 100,
+
+            helper: function(event) {
+                let original;
+                if ($(event.target).hasClass("ui-draggable")) {
+                    original = $(event.target);
+                } else {
+                    original = $(event.target).closest(".ui-draggable");
+                }
+                return original.clone().css({
+                    maxWidth: original.outerWidth(),
+                });
+            }
+        });
+    }
+
 
 }
 
-// Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
+    //Create a function to handle adding a new task
+    const handleAddTask = (event) => {
+        event.preventDefault();
 
-}
+        const task = {
+            id: generateTaskId(),
+            title: $("#taskTitle").val(),
+            description: $("#taskDescription").val(),
+            dueDate: $("#taskDueDate").val(),
+            status: 'to-do'
+        }
+        taskList.push(task);
+        localStorage.setItem("tasks", JSON.stringify(taskList));
+        renderTaskList();
 
-// Todo: create a function to handle adding a new task
-function handleAddTask(event){
+        $("#taskTitle").val("");
+        $("#taskDescription").val("");
+        $("#taskDueDate").val("");
+    }
 
-}
+    //Create a function to handle deleting a task
+    function handleDeleteTask(event) {
+        event.preventDefault();
+        const taskId = $(this).attr("data-task-id");
+        taskList = taskList.filter(task => task.id !== parseInt(taskId));
+        renderTaskList();
 
-// Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+    }
 
-}
+    //Create a function to handle dropping a task into a new status lane
+    const handleDrop = (event, ui) => {
+        const taskId = ui.draggable[0].dataset.taskId;
+        const newStatus = event.target.id;
 
-// Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
+        for (let index = 0; index < taskList.length; index++) {
+            if (taskList[index].id == parseInt(taskId)) {
+                taskList[index].status = newStatus;
+            }
+        }
+        localStorage.setItem("tasks", JSON.stringify(taskList));
+        renderTaskList();
 
-}
+    }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
+    //When the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+    $(document).ready(() => {
+        renderTaskList();
 
-});
+        $("#taskForm").on("submit", handleAddTask);
+
+        $(".lane").droppable({
+            accept: ".draggable",
+            drop: handleDrop
+        });
+
+        $("#taskDueDate").datepicker({
+            changeMonth: true,
+            changeYear: true
+        });
+    
+
+      
+    });
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Task Board Starter Code
+
+Task-Board :
+
+The motivation behind this project is categorizing tasks based on priority. This project reminds the user of tasks with higher priorities to be done, and once a task is completed, it moves to the completed section. Working on this project helped me learn jQuery and jQuery UI.
+
+
+## Installation
+
+The first step is to clone the project from GitHub, then paste it in the desired folder using the terminal or Git Bash, open it in the editor, and have access to the code.
+
+## Usage
+
+
+
+[alt text](./Develop/assets/images/screencapture-file-Users-mohammad-Desktop-Bootcamp-Challenges-task-board-index-html-2024-04-03-21_04_03.png)
+
+
+
+## License
+
+MIT License
+
+---
+
+
